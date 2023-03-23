3966fc400ddd0fdd987a9aa0e67fcf133d611504
Denis <denis@example.com>
2023-03-23 21:20:12 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+if (module.hot) {
+  module.hot.accept(function() {
+    window.location.reload();
+  });
+}
